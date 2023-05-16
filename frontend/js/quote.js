5e4b2bf87c1ba6234ca5f082af254d2b1619dde2
wooy0ng <yygg9800@naver.com>
2023-05-16 18:20:29 +0900

    feat: add bookmark interface
    - 북마크바 인터페이스를 추가하였습니다.
     - 저장 기능은 추후 추가할 예정입니다.
    
     #4

--- frontend/js/quote.js
+++ frontend/js/quote.js
@@ -1,33 +1,38 @@
-const quoteAPI_URL = "https://port-0-random-quote-4uvg2mleme84ru.sel3.cloudtype.app/";
-const quoteElement = document.getElementById("quote");
-const quoteItem = localStorage.getItem("quote");
+(function () {
+  const quoteAPI_URL =
+    "https://port-0-random-quote-4uvg2mleme84ru.sel3.cloudtype.app/";
+  const quoteElement = document.getElementById("quote");
+  const quoteItem = localStorage.getItem("quote");
 
-const nowDate = new Date();
-const month = nowDate.getMonth();
-const date = nowDate.getDate();
+  const nowDate = new Date();
+  const month = nowDate.getMonth();
+  const date = nowDate.getDate();
 
-const getQuote = async () => {
-  try {
-    const data = await fetch(quoteAPI_URL).then((res) => res.json());
-    const result = data[1].respond;
-    setQuote(result);
-  } catch (err) {
-    console.log(`err: ${err}`);
-    setQuote("시작이 반이다.");
-  }
-};
+  const getQuote = async () => {
+    try {
+      const data = await fetch(quoteAPI_URL).then((res) => res.json());
+      const result = data[1].respond;
+      setQuote(result);
+    } catch (err) {
+      console.log(`err: ${err}`);
+      setQuote("시작이 반이다.");
+    }
+  };
 
-const setQuote = (result) => {
-  let quote = { createDate: `${month}-${date}`, quoteData: result };
-  localStorage.setItem("quote", JSON.stringify(quote));
-  quoteElement.textContent = result;
-};
+  const setQuote = (result) => {
+    let quote = { createDate: `${month}-${date}`, quoteData: result };
+    localStorage.setItem("quote", JSON.stringify(quote));
+    quoteElement.textContent = result;
+  };
 
-if (quoteItem) {
-  let { createDate, quoteData } = JSON.parse(quoteItem);
-  if (createDate === `${month}-${date}`) {
-    quoteElement.textContent = `"${quoteData}"`;
+  if (quoteItem) {
+    let { createDate, quoteData } = JSON.parse(quoteItem);
+    if (createDate === `${month}-${date}`) {
+      quoteElement.textContent = `"${quoteData}"`;
+    } else {
+      getQuote();
+    }
   } else {
     getQuote();
   }
-}
+})();
--- frontend/js/search.js
+++ frontend/js/search.js
@@ -1,17 +1,19 @@
-const searchInputElement = document.getElementById("search-input");
+(function () {
+  const searchInputElement = document.getElementById("search-input");
 
-const showSearchResult = () => {
-  let searchWord = searchInputElement.value;
-  window.location.href = `https://www.google.com/search?q=${searchWord}`;
-  searchWord = "";
-};
+  const showSearchResult = () => {
+    let searchWord = searchInputElement.value;
+    window.location.href = `https://www.google.com/search?q=${searchWord}`;
+    searchWord = "";
+  };
 
-const searchEnterEvent = (event) => {
-  if (event.code === "Enter") {
-    showSearchResult();
-  }
-};
+  const searchEnterEvent = (event) => {
+    if (event.code === "Enter") {
+      showSearchResult();
+    }
+  };
 
-searchInputElement.addEventListener("keypress", (event) => {
-  searchEnterEvent(event);
-});
+  searchInputElement.addEventListener("keypress", (event) => {
+    searchEnterEvent(event);
+  });
+})();
